919a1088dc961942b191a93d216e70ad3692339b
Matheus Pazinati <matheuspazinati@gmail.com>
2022-11-25 05:10:04 -0300

    Cria um componente layout, que sera utilizado por todas as paginas da aplicacao

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
+import { DefaultLayout } from '../src/layouts/DefaultLayout'
+
 import { Cart } from './pages/Cart'
 import { Home } from './pages/Home'
 import { Order } from './pages/Order'
@@ -7,9 +9,11 @@ import { Order } from './pages/Order'
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/cart' element={<Cart />} />
-      <Route path='/order' element={<Order />} />
+      <Route path='/' element={<DefaultLayout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<Order />} />
+      </Route>
     </Routes>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,6 @@ import { Menu } from "../../components/Menu";
 
 export function Home() {
   return (
-    <Menu />
+    <h1>Home</h1>
   )
 }
