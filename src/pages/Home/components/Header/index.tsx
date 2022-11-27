69a0b7213e2fee61ed04f5c519867f40fc27c4c5
Matheus Pazinati <matheuspazinati@gmail.com>
2022-11-27 05:57:51 -0300

    Finaliza componente Header

--- src/pages/Home/components/Header/index.tsx
+++ src/pages/Home/components/Header/index.tsx
@@ -1,35 +1,43 @@
-import headerImage from '../../../../assets/cd-banner.png'
-
 import { ShoppingCart, Package, Timer, Coffee } from 'phosphor-react'
+
+import { BannerImage, HeaderContainer, HeaderContentContainer, HeaderIcon } from './styles'
 
 export function Header() {
   return (
-    <header>
-      <img src={headerImage} alt="" />
-      <div>
+    <HeaderContainer>
+      <BannerImage />
+      <HeaderContentContainer>
         <div>
           <h1>Encontre o café perfeito para qualquer hora do dia</h1>
           <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
         </div>
         <ul>
           <li>
-            <ShoppingCart size={16} />
+            <HeaderIcon backgroundVariant="yellow-dark">
+              <ShoppingCart size={16} weight={'fill'} />
+            </HeaderIcon>
             <span>Compra simples e segura</span>
           </li>
           <li>
-            <Package size={16} />
+            <HeaderIcon backgroundVariant="base-text">
+              <Package size={16} weight={'fill'} />
+            </HeaderIcon>
             <span>Embalagem mantém o café intacto</span>
           </li>
           <li>
-            <Timer size={16} />
+            <HeaderIcon backgroundVariant="yellow">
+              <Timer size={16} weight={'fill'} />
+            </HeaderIcon>
             <span>Entrega rápida e rastreada</span>
           </li>
           <li>
-            <Coffee size={16} />
+            <HeaderIcon backgroundVariant="purple">
+              <Coffee size={16} weight={'fill'}  />
+            </HeaderIcon>
             <span>O café chega quentinho até você</span>
           </li>
         </ul>
-      </div>
-    </header>
+      </HeaderContentContainer>
+    </HeaderContainer>
   )
 }
