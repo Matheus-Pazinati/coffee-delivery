8a88b08366ea7f507de75a8749680ef625c0e720
Matheus Pazinati <matheuspazinati@gmail.com>
2022-12-03 05:06:06 -0300

    Finaliza estilizacao pagina de confirmar pedido

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -9,33 +9,33 @@ import { OrderContainer, OrderContent, OrderStatus } from "./styles";
 export function Order() {
   return (
     <OrderContainer>
-      <OrderContent>
         <h2>Uhu! Pedido confirmado</h2>
         <p className="OrderSubtitle">Agora é só aguardar que logo o café chegará até você</p>
-        <OrderStatus>
-          <div>
-            <RoundedIcon Icon={MapPin} bgColor={'purple'} />
-            <p>
-              Entrega em <span> Rua João Daniel Martinelli, 102</span> <br /> Farrapos - Porto Alegre, RS
-            </p>
-          </div>
-          <div>
-            <RoundedIcon Icon={Timer} bgColor={'yellow'} />
-            <p>
-              Previsão de entrega <br />
-              <span> 20 min - 30 min </span>
-            </p>
-          </div>
-          <div>
-            <RoundedIcon Icon={CurrencyDollar} bgColor={'yellow-dark'} />
-            <p>
-              Pagamento na entrega <br />
-              <span>Cartão de Crédito</span>
-            </p>
-          </div>
-        </OrderStatus>
-      </OrderContent>
-      <img src={confirmImg} alt="" />
+        <OrderContent>
+          <OrderStatus>
+            <div>
+              <RoundedIcon Icon={MapPin} bgColor={'purple'} />
+              <p>
+                Entrega em <span> Rua João Daniel Martinelli, 102</span> <br /> Farrapos - Porto Alegre, RS
+              </p>
+            </div>
+            <div>
+              <RoundedIcon Icon={Timer} bgColor={'yellow'} />
+              <p>
+                Previsão de entrega <br />
+                <span> 20 min - 30 min </span>
+              </p>
+            </div>
+            <div>
+              <RoundedIcon Icon={CurrencyDollar} bgColor={'yellow-dark'} />
+              <p>
+                Pagamento na entrega <br />
+                <span>Cartão de Crédito</span>
+              </p>
+            </div>
+          </OrderStatus>
+            <img src={confirmImg} alt="" />
+          </OrderContent>
     </OrderContainer>
   )
 }
