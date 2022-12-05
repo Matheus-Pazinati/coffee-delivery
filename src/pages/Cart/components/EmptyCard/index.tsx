866ca6f72991d82eeebf28c003d892f6425da6d4
Matheus Pazinati <matheuspazinati@gmail.com>
2022-12-05 04:19:41 -0300

    Cria estilizacao do componente de EmptyCart para dispositivos moveis

--- src/pages/Cart/components/EmptyCard/index.tsx
+++ src/pages/Cart/components/EmptyCard/index.tsx
@@ -1,11 +1,12 @@
 import emptyCartImg from '../../../../assets/cd-empty-cart.png'
+import { EmptyCartContainer } from './styles'
 
 export function EmptyCard() {
   return (
-    <div>
+    <EmptyCartContainer>
       <img src={emptyCartImg} alt="Desenho de um carrinho de compras vazio" />
       <p>Seu carrinho não possui nenhum item.</p>
       <button>Escolher um café</button>
-    </div>
+    </EmptyCartContainer>
   )
 }
--- src/pages/Cart/components/EmptyCard/styles.ts
+++ src/pages/Cart/components/EmptyCard/styles.ts
@@ -1,1 +1,30 @@
 import styled from "styled-components";
+
+export const EmptyCartContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1.25rem;
+  margin-top: 2rem;
+  padding: 2rem;
+  background-color: ${({theme}) => theme["base-card"]};
+  border-radius: 6px 44px;
+
+  img {
+    width: 50%;
+    max-width: 300px;
+  }
+
+  P {
+    font-weight: 700;
+  }
+
+  button {
+    padding: 1rem;
+    background-color: ${({theme}) => theme.yellow};
+    font-size: 1rem;
+    color: ${({theme}) => theme.white};
+    border-radius: 6px;
+  }
+`
