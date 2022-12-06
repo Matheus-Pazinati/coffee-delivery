9195ad611eed066ce1134375f1023d1f0f05070f
Matheus Pazinati <matheuspazinati@gmail.com>
2022-12-06 05:21:08 -0300

    Cria estado para receber os dados da API que armazena as informacoes de cada cafe no menu

--- src/pages/Home/components/CoffeeMenu/index.tsx
+++ src/pages/Home/components/CoffeeMenu/index.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
 import { CoffeeCard } from "../CoffeeCard";
 
 import { CoffeeItems, CoffeeMenuContainer } from "./styles";
 
+export interface CoffeeCardProps {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  tags: string[];
+  price: number
+}
+
 export function CoffeeMenu() {
+  const url = "https://my-json-server.typicode.com/Matheus-Pazinati/coffee-delivery-json-db/coffee";
+
+  const [coffee, setCoffee] = useState<CoffeeCardProps[]>([])
+
+  useEffect(() => {
+    async function fetchCoffeMenuData() {
+      const response = await fetch(url)
+      const jsonResponse = await response.json() as CoffeeCardProps[]
+      setCoffee(jsonResponse)
+    }
+
+    fetchCoffeMenuData()
+  }, [])
+
   return (
     <CoffeeMenuContainer>
       <h2>Nossos cafés</h2>
