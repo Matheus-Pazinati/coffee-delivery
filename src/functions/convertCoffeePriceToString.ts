c95aea0bb846af9709bba72c412ae52227c6531e
Matheus Pazinati <matheuspazinati@gmail.com>
2022-12-10 04:12:26 -0300

    Corrige erro na funcao de converter o valor do cafe para string

--- src/functions/convertCoffeePriceToString.ts
+++ src/functions/convertCoffeePriceToString.ts
@@ -1,6 +1,6 @@
 export function convertCoffeePriceToString(price: number, quantity: number) {
   const coffeeTotalPrice = price * quantity
-  const coffeePriceView = coffeeTotalPrice.toFixed(1).replace('.', ',').concat('0')
+  const coffeePriceView = coffeeTotalPrice.toFixed(2).replace('.', ',')
 
   return coffeePriceView
 }
