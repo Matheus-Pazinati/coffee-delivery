47e4dff8fc301cd662d1ef4c5851cad9b7ae5be5
Matheus Pazinati <matheuspazinati@gmail.com>
2022-12-10 05:12:28 -0300

    Verifica se o item ja esta no carrinho, caso sim aumente a quantidade, caso nao o adicione

--- src/reducer/coffee/reducer.ts
+++ src/reducer/coffee/reducer.ts
@@ -5,10 +5,23 @@ import { ActionTypes } from "./actions";
 
 export function coffeesReducer(state: SelectedCoffee[], action: any) {
   switch(action.type) {
-    case ActionTypes.ADD_NEW_COFFEE: 
-      return produce(state, (draft) => {
-        draft.push(action.payload.coffee)
+    case ActionTypes.ADD_NEW_COFFEE: {
+
+      const coffeeOnCartIndex = state.findIndex((coffee) => {
+        return coffee.id === action.payload.coffee.id
       })
+      const coffeeIsInTheCart = coffeeOnCartIndex >= 0
+
+      return produce(state, (draft) => {
+        if (coffeeIsInTheCart) {
+          draft[coffeeOnCartIndex].quantity += 1
+          
+        } else {
+            draft.push(action.payload.coffee)
+          }
+      })
+    }
+
   }
   return state
 }
