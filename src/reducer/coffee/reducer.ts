58b88415dc792aa7d344e2e916541565c8c1bfce
Matheus Pazinati <matheuspazinati@gmail.com>
2022-12-12 03:31:27 -0300

    Reaproveita metodo para encontrar o indice de um cafe em uma lista com base no id

--- src/reducer/coffee/reducer.ts
+++ src/reducer/coffee/reducer.ts
@@ -4,17 +4,22 @@ import { SelectedCoffee } from "../../context/CoffeeContext";
 import { ActionTypes } from "./actions";
 
 export function coffeesReducer(state: SelectedCoffee[], action: any) {
+
+  const coffeeOnCartIndex = state.findIndex((coffee) => {
+    return coffee.id === action.payload.id
+  })
+
   switch(action.type) {
     case ActionTypes.ADD_NEW_COFFEE: {
 
-      const coffeeOnCartIndex = state.findIndex((coffee) => {
+      const coffeeIndexInTheCart = state.findIndex((coffee) => {
         return coffee.id === action.payload.coffee.id
       })
-      const coffeeIsInTheCart = coffeeOnCartIndex >= 0
+      const coffeeIsInTheCart = coffeeIndexInTheCart >= 0
 
       return produce(state, (draft) => {
         if (coffeeIsInTheCart) {
-          draft[coffeeOnCartIndex].quantity += 1
+          draft[coffeeIndexInTheCart].quantity += 1
           
         } else {
             draft.push(action.payload.coffee)
@@ -23,10 +28,6 @@ export function coffeesReducer(state: SelectedCoffee[], action: any) {
     }
 
     case ActionTypes.INCREMENT_COFFEE_QUANTITY: {
-      const coffeeOnCartIndex = state.findIndex((coffee) => {
-        return coffee.id === action.payload.id
-      })
-
       return produce(state, (draft) => {
         if (coffeeOnCartIndex < 0) {
           return state
@@ -36,10 +37,6 @@ export function coffeesReducer(state: SelectedCoffee[], action: any) {
     }
 
     case ActionTypes.DECREMENT_COFFEE_QUANTITY: {
-      const coffeeOnCartIndex = state.findIndex((coffee) => {
-        return coffee.id === action.payload.id
-      })
-
       return produce(state, (draft) => {
         if (coffeeOnCartIndex < 0) {
           return state
@@ -49,10 +46,6 @@ export function coffeesReducer(state: SelectedCoffee[], action: any) {
     }
 
     case ActionTypes.REMOVE_COFFEE_CART: {
-      const coffeeOnCartIndex = state.findIndex((coffee) => {
-        return coffee.id === action.payload.id
-      })
-
       return produce(state, (draft) => {
         if (coffeeOnCartIndex < 0) {
           return state
